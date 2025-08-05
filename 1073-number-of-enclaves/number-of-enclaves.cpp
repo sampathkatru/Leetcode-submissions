f2765bb5c7e85b1c562f274002bce9a969ce5f45
SaMPaTH VISHaL K <94291973+sampathkatru@users.noreply.github.com>
2025-08-05 12:31:40 +0530

    Time: 7 ms (76.85%) | Memory: 34.3 MB (52.89%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,46 +1,35 @@
 class Solution {
-public:
-    int check(vector<vector<int>>& grid, int i, int j, int n, int m, bool &flag) {
-        stack<pair<int, int>> stk;
-        stk.push({i, j});
+private:
+    void dfs(std::vector<std::vector<int>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >=grid[0].size() || grid[i][j] != 1) return;
+
         grid[i][j] = -1;
-        int count = 1;
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        const int m = grid.size();
+        const int n = grid[0].size();
+        for (int i = 0; i < m; ++i) {
+            dfs(grid, i , 0);
+            dfs(grid, i, n-1);
+        }
 
-        if(i == 0 || j == 0 || i == n-1 || j == m-1) flag = true;
+        for (int i = 0; i < n; ++i) {
+            dfs(grid, 0, i);
+            dfs(grid, m-1, i);
+        }
 
-        vector<pair<int, int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
-        while(!stk.empty()) {
-            auto [x, y] = stk.top();
-            stk.pop();
-
-            for(auto [dx, dy] : dirs) {
-                int nx = x + dx, ny = y + dy;
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] == 1) {
-                    if(nx == 0 || ny == 0 || nx == n-1 || ny == m-1) flag = true;
-                    grid[nx][ny] = -1;
-                    stk.push({nx, ny});
-                    count++;
-                }
+        int count = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == 1) ++count;
             }
         }
 
         return count;
-    }
-
-    int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size(), result = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1) {
-                    bool flag = false;
-                    int val = check(grid, i, j, n, m, flag);
-                    if(!flag) result += val;
-                }
-            }
-        }
-
-        return result;
     }
 };
