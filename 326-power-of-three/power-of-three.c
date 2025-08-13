ab55f889908ee0aeada8183ca61a46a1d1624dfb
SaMPaTH VISHaL K <94291973+sampathkatru@users.noreply.github.com>
2025-08-13 06:07:48 +0530

    Time: 3 ms (59.38%) | Memory: 8.4 MB (92.33%) - LeetSync

---
+++ 326-power-of-three/power-of-three.c
@@ -0,0 +1,19 @@
+bool isPowerOfThree(int n) 
+{
+   if(n==0)
+   {
+    return false;
+   }    
+   if(n==1)
+   {
+    return true;
+   }
+   if(n>1)
+   {
+    return n%3==0 && isPowerOfThree(n/3); 
+   }
+   else
+   {
+    return false;
+   }
+}
